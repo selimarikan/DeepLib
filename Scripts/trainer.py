2bc02f83ff9760fb61832a71845a2d378dbbb934
selimarikan <selimarikan@gmail.com>
2018-08-13 20:11:43 +0200

    Added MXNet trainer file. Added config system

--- Scripts/trainer.py
+++ Scripts/trainer.py
@@ -3,7 +3,7 @@ import torch
 import torchvision
 import numpy as np
 
-def HandleArguments():
+def ProcessArguments():
     argParser = argparse.ArgumentParser()
     argParser.add_argument("--dataset", required = True, type = str, help = "Path to the dataset images")
     argParser.add_argument("--model", required = True, type = str, help = "Model to use for training")
@@ -11,6 +11,40 @@ def HandleArguments():
     argParser.add_argument("--epochs", required = True, type = int, help = "How many epochs for training")
     argParser.add_argument("--imagesize", required = True, type = int, help = "Size of the image for training")
 
+class LearningConfig(object):
+    def __init__(self, datasetPath, modelName, optimizerName, optimizerState, epoch, imageSize, batchSize, lr, l2, lrDecay):
+        self.datasetPath = datasetPath
+        self.modelName = modelName
+        self.optimizerName = optimizerName
+        self.optimizerState = optimizerState
+        self.epoch = epoch
+        self.imageSize = imageSize
+        self.batchSize = batchSize
+        self.lr = lr
+        self.l2 = l2
+        self.lrDecay = lrDecay
+    
+    def GetDict(self):
+        return {
+        'DatasetPath' : self.datasetPath,
+        'ModelName' : self.modelName,
+        'OptimizerName' : self.optimizerName,
+        'OptimizerState' : self.optimizerState,
+        'Epoch' : self.epoch,
+        'ImageSize' : self.imageSize,
+        'BatchSize' : self.batchSize,
+        'LearningRate' : self.lr,
+        'L2' : self.l2,
+        'LRDecay' : self.lrDecay,
+        }
+
+def CreateConfig():
+    configDict = {
+        
+
+
+    }
+
 
 if __name__ == "__main__":
-    HandleArguments()
+    ProcessArguments()
--- Scripts/trainer_mxnet.py
+++ Scripts/trainer_mxnet.py
@@ -2,6 +2,31 @@ import mxnet as mx
 from mxnet import gluon
 from mxnet.gluon import nn
 from mxnet import autograd as ag
+import mxnet.ndarray as F
+
+class Net(gluon.Block):
+    def __init__(self, **kwargs):
+        super(Net, self).__init__(**kwargs)
+
+        with self.name_scope():
+            # layers created in name_scope will inherit name space
+            # from parent layer.
+            self.conv01 = nn.Conv2D(channels=20, kernel_size=(5, 5))
+            self.pool01 = nn.MaxPool2D(pool_size=(2, 2), strides=(2, 2))
+            self.conv02 = nn.Conv2D(channels=50, kernel_size=(5, 5))
+            self.pool02 = nn.MaxPool2D(pool_size=(2, 2), strides=(2, 2))
+            self.fc01 = nn.Dense(units=500)
+            self.fc02 = nn.Dense(units=10)
+    
+    def forward(self, x):
+        x = self.pool01(F.tanh(self.conv01(x)))
+        x = self.pool02(F.tanh(self.conv02(x)))
+        # 0 means copy over size from corresponding dimension.
+        # -1 means infer size from the rest of dimensions.
+        x = x.reshape(0, -1)
+        x = F.tanh(self.fc01(x))
+        x = F.tanh(self.fc02(x))
+        return x
 
 
 def GetMNIST():
@@ -19,6 +44,7 @@ def GetMLPModel():
         net.add(nn.Dense(units=10))
 
     return net
+
 
 def Train(ctx, model, numEpochs, trainData, trainer, lossFunc, metric):
     for i in range(numEpochs):
@@ -68,7 +94,8 @@ if __name__ == '__main__':
     mx.random.seed(0)
 
     mnist = GetMNIST()
-    model = GetMLPModel()
+    #model = GetMLPModel()
+    model = Net()
     GPUs = mx.test_utils.list_gpus()
 
     batchSize = 100
