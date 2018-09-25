6ac4d4b14d8d6a4fd1685887419b422f4965d4ed
selim <selimarikan@gmail.com>
2018-09-25 23:13:35 +0200

    removed unused dictionary

--- Scripts/trainer.py
+++ Scripts/trainer.py
@@ -1,7 +1,12 @@
+import os
 import argparse
 import json
 import torch
+import torch.nn as nn
+import torch.optim as optim
 import torchvision
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
 import numpy as np
 
 configPath = './config.json'
@@ -25,6 +30,7 @@ def ProcessArguments():
     argParser.add_argument("--epochs", required = True, type = int, help = "How many epochs for training")
     argParser.add_argument("--imagesize", required = True, type = int, help = "Size of the image for training")
 
+
 class LearningConfig(object):
     def __init__(self, datasetPath, modelName, optimizerName, optimizerState, epoch, imageSize, batchSize, lr, l2, lrDecay):
         self.datasetPath = datasetPath
@@ -37,20 +43,28 @@ class LearningConfig(object):
         self.lr = lr
         self.l2 = l2
         self.lrDecay = lrDecay
-    
-    def GetDict(self):
-        return {
-        'DatasetPath' : self.datasetPath,
-        'ModelName' : self.modelName,
-        'OptimizerName' : self.optimizerName,
-        'OptimizerState' : self.optimizerState,
-        'Epoch' : self.epoch,
-        'ImageSize' : self.imageSize,
-        'BatchSize' : self.batchSize,
-        'LearningRate' : self.lr,
-        'L2' : self.l2,
-        'LRDecay' : self.lrDecay,
-        }
+
+
+def CreateTrafos(imageSize, cropSize, setMean, setStd, onlineAug = False):
+    trafos = {
+        'train': transforms.Compose([
+            transforms.Random
+        ])
+    }
+
+def CreateDataset(dataDir, trafo):
+    datasets = {x: datasets.ImageFolder(os.path.join(dataDir, x), trafo[x]) for x in ['train', 'test']}
+
+
+def Train():
+    pass
+
+
+
+
+
+
+
 
 def CreateConfig():
     configDict = {
